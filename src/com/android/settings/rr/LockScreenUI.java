a34b2aabee76704bbf30abd71f1ada07defca07c
Alex Cruz <alex@dirtyunicorns.com>
2018-05-28 13:50:00 +0200

    settings: [SQUASH] Lockscreen customization [2/2]
    Includes the following commits:
    
    * Lockscreen Clock Widget Fonts [2/2]
    by @Alex Cruz
    from: https://github.com/BootleggersROM/packages_apps_BootlegDumpster/commit/be1ceb0c1399991f7cd603a5509dfe197b13dc8f
    
    * Add the new fonts to the Lockscreen font selector [2/2]
    by @eldainosor
    from: https://github.com/BootleggersROM/packages_apps_BootlegDumpster/commit/177e7a23e6482884ba226d2f454e4ca2aceb38c1
    
    * Add More Fonts for Lock Clock. Courtsey - Varun Date RR
    by @SuperDroidBond
    from: https://github.com/Project-Xtended/packages_apps_Xtensions/commit/dd9d6da0cfb33251ecf51ac590ca6452394b88d4
    
    * Lockscreen Date Fonts [2/2]
    by @SuperDroidBond
    from: https://github.com/Project-Xtended/packages_apps_Xtensions/commit/e0fb09c954cc36ed0f4f2128e64fccd2cb10dfa7
    
    * Add Lockscreen Date n Clock Sizes [2/2]
    by @SuperDroidBond
    from: https://github.com/Project-Xtended/packages_apps_Xtensions/commit/c24a5d4b6c085f6f3f3cb58d354296e06c306067
    
    @mcdachpappe:
    * adapted to work on resurrection-remix v6.0.0
    * re-organize lockscreen-ui-settings
    * fix some misc stuff
    
    Change-Id: Ibe641bbb1a04ac5c16e5643cb4cc999ab97c2097
    Co-authored-by: El Dainosor <eldainosor@gmail.com>
    Co-authored-by: SuperDroidBond <superdroidbond@yahoo.com>

--- src/com/android/settings/rr/LockScreenUI.java
+++ src/com/android/settings/rr/LockScreenUI.java
@@ -14,6 +14,7 @@
 package com.android.settings.rr;
 
 import android.content.ContentResolver;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.provider.Settings;
@@ -27,14 +28,24 @@ import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.rr.Preferences.CustomSeekBarPreference;
 import com.android.settings.Utils;
 
 public class LockScreenUI extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "LockScreenSecurity";
+    private static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
+    private static final String LOCK_DATE_FONTS = "lock_date_fonts";
+    private static final String CLOCK_FONT_SIZE  = "lockclock_font_size";
+    private static final String DATE_FONT_SIZE  = "lockdate_font_size";
     private static final String KEY_LOCKSCREEN_CLOCK_SELECTION = "lockscreen_clock_selection";
     private static final String KEY_LOCKSCREEN_DATE_SELECTION = "lockscreen_date_selection";
 
+    ListPreference mLockClockFonts;
+    ListPreference mLockDateFonts;
+
+    private CustomSeekBarPreference mClockFontSize;
+    private CustomSeekBarPreference mDateFontSize;
     private ListPreference mLockscreenClockSelection;
     private ListPreference mLockscreenDateSelection;
 
@@ -51,7 +62,35 @@ public class LockScreenUI extends SettingsPreferenceFragment implements
 
 	ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
+	Resources resources = getResources();
 
+	// Lockscren Clock Fonts
+        mLockClockFonts = (ListPreference) findPreference(LOCK_CLOCK_FONTS);
+        mLockClockFonts.setValue(String.valueOf(Settings.System.getInt(
+                getContentResolver(), Settings.System.LOCK_CLOCK_FONTS, 4)));
+        mLockClockFonts.setSummary(mLockClockFonts.getEntry());
+        mLockClockFonts.setOnPreferenceChangeListener(this);
+
+	// Lockscren Date Fonts
+        mLockDateFonts = (ListPreference) findPreference(LOCK_DATE_FONTS);
+        mLockDateFonts.setValue(String.valueOf(Settings.System.getInt(
+                getContentResolver(), Settings.System.LOCK_DATE_FONTS, 4)));
+        mLockDateFonts.setSummary(mLockDateFonts.getEntry());
+        mLockDateFonts.setOnPreferenceChangeListener(this);
+
+	// Lockscreen Clock font Size
+	mClockFontSize = (CustomSeekBarPreference) findPreference(CLOCK_FONT_SIZE);
+        mClockFontSize.setValue(Settings.System.getInt(getContentResolver(),
+                Settings.System.LOCKCLOCK_FONT_SIZE, 90));
+        mClockFontSize.setOnPreferenceChangeListener(this);
+
+	// Lockscreen Date font Size
+        mDateFontSize = (CustomSeekBarPreference) findPreference(DATE_FONT_SIZE);
+        mDateFontSize.setValue(Settings.System.getInt(getContentResolver(),
+                Settings.System.LOCKDATE_FONT_SIZE, 24));
+        mDateFontSize.setOnPreferenceChangeListener(this);
+
+	// Lockscreen Clock Style
     	mLockscreenClockSelection = (ListPreference) findPreference(KEY_LOCKSCREEN_CLOCK_SELECTION);
         int clockSelection = Settings.System.getIntForUser(resolver,
                 Settings.System.LOCKSCREEN_CLOCK_SELECTION, 0, UserHandle.USER_CURRENT);
@@ -59,19 +98,41 @@ public class LockScreenUI extends SettingsPreferenceFragment implements
         mLockscreenClockSelection.setSummary(mLockscreenClockSelection.getEntry());
         mLockscreenClockSelection.setOnPreferenceChangeListener(this);
 
+	// Lockscreen Date Style
         mLockscreenDateSelection = (ListPreference) findPreference(KEY_LOCKSCREEN_DATE_SELECTION);
         int dateSelection = Settings.System.getIntForUser(resolver,
                 Settings.System.LOCKSCREEN_DATE_SELECTION, 0, UserHandle.USER_CURRENT);
         mLockscreenDateSelection.setValue(String.valueOf(dateSelection));
         mLockscreenDateSelection.setSummary(mLockscreenDateSelection.getEntry());
         mLockscreenDateSelection.setOnPreferenceChangeListener(this);
-
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
 	ContentResolver resolver = getActivity().getContentResolver();
-	if (preference == mLockscreenClockSelection) {
+	if (preference == mLockClockFonts) {
+            Settings.System.putInt(getContentResolver(), Settings.System.LOCK_CLOCK_FONTS,
+                    Integer.valueOf((String) newValue));
+            mLockClockFonts.setValue(String.valueOf(newValue));
+            mLockClockFonts.setSummary(mLockClockFonts.getEntry());
+            return true;
+	} else if (preference == mLockDateFonts) {
+            Settings.System.putInt(getContentResolver(), Settings.System.LOCK_DATE_FONTS,
+                    Integer.valueOf((String) newValue));
+            mLockDateFonts.setValue(String.valueOf(newValue));
+            mLockDateFonts.setSummary(mLockDateFonts.getEntry());
+            return true;
+	} else if (preference == mClockFontSize) {
+            int top = (Integer) newValue;
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.LOCKCLOCK_FONT_SIZE, top*1);
+            return true;
+        } else if (preference == mDateFontSize) {
+            int top = (Integer) newValue;
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.LOCKDATE_FONT_SIZE, top*1);
+            return true;
+	} else if (preference == mLockscreenClockSelection) {
             int clockSelection = Integer.valueOf((String) newValue);
             int index = mLockscreenClockSelection.findIndexOfValue((String) newValue);
             Settings.System.putIntForUser(resolver,
