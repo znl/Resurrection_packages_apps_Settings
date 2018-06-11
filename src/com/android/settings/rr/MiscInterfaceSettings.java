899d56a219754444d2ab48088bc304004ca1fbeb
AgentFabulous <kshitijgm@gmail.com>
2018-06-11 18:00:27 +0200

    [SQUASH] Allow using framework values for rounded corners [2/2]
    this commit contains:
    
    * Settings: Add Runtime Rounded Corner preferences
    by @AgentFabulous
    from here: https://github.com/AIMROM/packages_apps_freedomhub/commit/6129955ce50681d1befe331603c865f9e4c193af
    
    * wedges: Allow using framework values for rounded corners [2/2]
    by @AgentFabulous
    from here: https://github.com/AIMROM/packages_apps_freedomhub/commit/9cbdf4adf10c281e44ac45fac81e43954864b246
    
    @mcdachpappe:
    * place it in our settings/misc_interface_ui
    * adapt to work on resurrection-remix
    
    Change-Id: I560ec5ada8e126eb939858d6e20d1aec4b5caa03

--- src/com/android/settings/rr/MiscInterfaceSettings.java
+++ src/com/android/settings/rr/MiscInterfaceSettings.java
@@ -13,9 +13,11 @@
 */
 package com.android.settings.rr;
 
+import android.content.Context;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.ServiceManager;
 import android.provider.Settings;
 import android.support.v7.preference.ListPreference;
 import android.support.v14.preference.SwitchPreference;
@@ -23,11 +25,18 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceScreen;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
+import android.util.Log;
+
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
+import com.android.settings.rr.Preferences.SecureSettingSeekBarPreference;
 
 public class MiscInterfaceSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -35,6 +44,14 @@ public class MiscInterfaceSettings extends SettingsPreferenceFragment implements
     private static final String RR_OTA = "rr_ota_fab";
 
     private SwitchPreference mConfig;
+
+    // Rounded Ui
+    private static final String SYSUI_ROUNDED_SIZE = "sysui_rounded_size";
+    private static final String SYSUI_ROUNDED_CONTENT_PADDING = "sysui_rounded_content_padding";
+
+    private Context mContext;
+    private SecureSettingSeekBarPreference mCornerRadius;
+    private SecureSettingSeekBarPreference mContentPadding;
 
     @Override
     public int getMetricsCategory() {
@@ -50,6 +67,20 @@ public class MiscInterfaceSettings extends SettingsPreferenceFragment implements
         mConfig.setChecked((Settings.System.getInt(getContentResolver(),
                             Settings.System.RR_OTA_FAB, 1) == 1));
         mConfig.setOnPreferenceChangeListener(this);
+
+	// Rounded Corner Radius
+        mCornerRadius = (SecureSettingSeekBarPreference) findPreference(SYSUI_ROUNDED_SIZE);
+        int cornerRadius = Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.SYSUI_ROUNDED_SIZE, 0);
+        mCornerRadius.setValue(cornerRadius / 1);
+        mCornerRadius.setOnPreferenceChangeListener(this);
+
+        // Rounded Content Padding
+        mContentPadding = (SecureSettingSeekBarPreference) findPreference(SYSUI_ROUNDED_CONTENT_PADDING);
+        int contentPadding = Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.SYSUI_ROUNDED_CONTENT_PADDING, 0);
+        mContentPadding.setValue(contentPadding / 1);
+        mContentPadding.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -63,6 +94,16 @@ public class MiscInterfaceSettings extends SettingsPreferenceFragment implements
             fabIntent.setClassName("com.android.settings", "com.android.settings.Settings$MainSettingsLayoutActivity");
             startActivity(fabIntent);
             return true;
+        } else if (preference == mCornerRadius) {
+            int value = (Integer) objValue;
+            Settings.Secure.putInt(getContentResolver(),
+                Settings.Secure.SYSUI_ROUNDED_SIZE, value * 1);
+	    return true;
+        } else if (preference == mContentPadding) {
+            int value = (Integer) objValue;
+            Settings.Secure.putInt(getContentResolver(),
+                Settings.Secure.SYSUI_ROUNDED_CONTENT_PADDING, value * 1);
+	    return true;
         }
         return false;
     }
